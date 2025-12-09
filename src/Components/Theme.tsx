086bf66b243ad7fc4bb2fffd4f4fedf205a5822f
Gáspár Csanád <csanad0502@gmail.com>
2025-12-09 01:01:53 +0100

    Base theme changed from light to dark

--- src/Components/Theme.tsx
+++ src/Components/Theme.tsx
@@ -3,41 +3,42 @@ import {MoonIcon, SunIcon} from "lucide-react";
 import {cn} from "../lib/utils";
 
 export const Theme = () => {
-    const [isDarkmode, setIsDarkmode] = useState(() => {
+    const [isLightMode, setIsLightMode] = useState(() => {
         if (typeof window !== "undefined") {
-            return localStorage.getItem("theme") === "dark"
+            return localStorage.getItem("theme") === "light"
         }
         return true
     })
 
     useEffect(() => {
-        if(isDarkmode){
-            document.documentElement.classList.add("dark")
-            localStorage.setItem("theme", "dark")
-        }
-        else{
+        if(isLightMode){
             document.documentElement.classList.add("light")
             localStorage.setItem("theme", "light")
+        }
+        else{
+            document.documentElement.classList.add("dark")
+            localStorage.setItem("theme", "dark")
         }
     }, [])
 
     const toggleTheme = () => {
-        if(isDarkmode){
-            document.documentElement.classList.remove("dark")
-            localStorage.setItem("theme", "light")
-            setIsDarkmode(false)
-        }else{
+        if(isLightMode){
+            document.documentElement.classList.remove("light")
             document.documentElement.classList.add("dark")
             localStorage.setItem("theme", "dark")
-            setIsDarkmode(true)
+            setIsLightMode(false)
+        }else{
+            document.documentElement.classList.remove("dark")
+            localStorage.setItem("theme", "light")
+            setIsLightMode(true)
         }
     }
 
     return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden",
         "top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300")}>
-        {isDarkmode ?
-        <SunIcon className={"h-6 w-6 text-yellow-300"}/>
+        {isLightMode ?
+        <MoonIcon className={"h-6 w-6 text-blue-900"}/>
         :
-        <MoonIcon className={"h-6 w-6 text-blue-900"}/>}
+        <SunIcon className={"h-6 w-6 text-yellow-300"}/>}
     </button>
 }
